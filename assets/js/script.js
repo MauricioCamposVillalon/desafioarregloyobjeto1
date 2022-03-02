b2c346bff188482c7b5e2c0672d153159560f960
Mauricio Campos <thoyferr@gmail.com>
2022-03-01 21:32:59 -0300

    ingreso

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+
+
+let radiologia = [{
+    hora: '11:00',
+    especialista: 'IGNACIO SCHULZ',
+    paciente: 'FRANCISCA ROJAS',
+    rut: '9878782-1',
+    prevision: 'FONASA',
+},
+{
+    hora: '11:30',
+    especialista: 'FEDERICO SUBERCASEAUX',
+    paciente: 'PAMELA ESTRADA',
+    rut: '15345241-3',
+    prevision: 'ISAPRE',
+},
+{
+    hora: '15:00',
+    especialista: 'FERNANDO WURTHZ',
+    paciente: 'ARMANDO LUNA',
+    rut: '16445345-9',
+    prevision: 'ISAPRE',
+},
+{
+    hora: '15:30',
+    especialista: 'ANA MARIA GODOY',
+    paciente: 'MANUEL GODOY',
+    rut: '17666419-0',
+    prevision: 'FONASA',
+},
+{
+    hora: '16:00',
+    especialista: 'PATRICIA SUAZO',
+    paciente: 'RAMON ULLOA',
+    rut: '14989389-K',
+    prevision: 'FONASA',
+}];
+
+let traumatologia = [{
+    hora: '08:00',
+    especialista: 'PATRICIA SUAZO',
+    paciente: 'PAULA SANCHEZ',
+    rut: '15554774-5',
+    prevision: 'FONASA',
+},{
+    hora: '10:00',
+    especialista: 'RAUL ARAYA',
+    paciente: 'ANGÉLICA NAVAS',
+    rut: '15444147-9',
+    prevision: 'ISAPRE',
+}
+];
+
+
+
+
+console.log(radiologia);
+
+
